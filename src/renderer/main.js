a92e3db2c51385e46096dab9516ee5ba059a6c30
Bartosz Adamczyk <born2perform1@gmail.com>
2016-12-03 15:22:20 +0100

    feature
    Use react for managing view

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,9 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const fs = require('fs');
-fs.writeFileSync('./test', 'hello electron');
+import App from './App';
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<App />, document.querySelector('.js-main'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   entry: './src/renderer/main.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
   output: {
+    path: './dist',
     filename: 'renderer.js'
   },
   target: 'electron-renderer'
