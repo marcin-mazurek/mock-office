f51636d193abe164f7b116b0382137f1ee9727d9
Bartosz Adamczyk <born2perform1@gmail.com>
2016-12-03 16:23:59 +0100

    refactor
    eslint fix

--- src/renderer/App/App.js
+++ src/renderer/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const App = () => {
-    return <div>Hello mockee!</div>;
-};
+const App = () => (
+  <div>Hello mockee!</div>
+);
 
 export default App;
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,9 +1,5 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-import App from './App';
 import React from 'react';
 import { render } from 'react-dom';
+import App from './App';
 
 render(<App />, document.querySelector('.js-main'));
-
