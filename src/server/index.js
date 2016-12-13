08d6600910ad1438bbf03f8e3805468381aa1f12
Bartosz Adamczyk <born2perform1@gmail.com>
2016-12-13 16:13:35 +0100

    feat
    Response only to registered endpoints

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,38 @@
 import express from 'express';
 
+const endpoints = {
+  '/some-url': {
+    payload: {
+      value: 'Im JSON!'
+    }
+  }
+};
+
 export default class MockServer {
   constructor() {
     this.app = express();
     this.live = false;
 
     this.app.get('/*', (req, res) => {
-      res.send('Hi from mockee!');
+      const payload = this.preparePayload(req);
+
+      if (payload) {
+        res.set('Content-Type', 'application/json');
+        res.send(payload);
+      } else {
+        res.status(404).end();
+      }
     });
+  }
+
+  preparePayload(req) {
+    const endpoint = endpoints[req.url];
+
+    if (endpoint) {
+      return endpoint.payload;
+    }
+
+    return undefined;
   }
 
   start(cb) {
