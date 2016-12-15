c66b5365de2c6ab950d4b9b889ac982234254c2b
Bartosz Adamczyk <born2perform1@gmail.com>
2016-12-15 10:40:16 +0100

    feat
    Add ids to mocks after read from file

--- src/main/persistent/index.js
+++ src/main/persistent/index.js
@@ -1,3 +1,10 @@
+import uniqueId from 'node-unique';
+
 const fs = require('fs');
 
-export default (path) => JSON.parse(fs.readFileSync(path));
+export default path => JSON.parse(fs.readFileSync(path))
+  .map((mockFromFile) => {
+    const mock = mockFromFile;
+    mock.id = uniqueId();
+    return mock;
+  });
