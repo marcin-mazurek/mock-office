64a728a9ae7dd899a6f049118a7b9fc10af9132f
Bartosz Adamczyk <born2perform1@gmail.com>
2016-12-22 10:43:30 +0100

    refactor Improve store initialization code

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,19 +1,18 @@
 import createSagaMiddleware from 'redux-saga';
 import { fromJS } from 'immutable';
 import { createStore, compose, applyMiddleware } from 'redux';
-import { combineReducers } from 'redux-immutable';
 import { browserHistory } from 'react-router';
 import { routerMiddleware as createRouterMiddleware } from 'react-router-redux';
-import reducers from './reducers';
+import rootReducer from './rootReducer';
 import rootSaga from './rootSaga';
 
 export default () => {
   const initialState = fromJS({});
-  const reducer = combineReducers(reducers);
   const sagaMiddleware = createSagaMiddleware();
   const routerMiddleware = createRouterMiddleware(browserHistory);
   const enhancer = compose(applyMiddleware(sagaMiddleware, routerMiddleware));
-  const store = createStore(reducer, initialState, enhancer);
+  const store = createStore(rootReducer, initialState, enhancer);
   sagaMiddleware.run(rootSaga);
+
   return store;
 };
--- src/renderer/store/rootReducer.js
+++ src/renderer/store/rootReducer.js
@@ -1,9 +1,10 @@
+import { combineReducers } from 'redux-immutable';
 import mocksReducer from '../mocks';
 import routingReducer from './utils';
 import serversReducer from '../servers/reducer';
 
-export default {
+export default combineReducers({
   mocks: mocksReducer,
   routing: routingReducer,
   servers: serversReducer
-};
+});
