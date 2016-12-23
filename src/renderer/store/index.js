a75547ab941610989d6df4a6b2fa59d07168fe82
Bartosz Adamczyk <born2perform1@gmail.com>
2016-12-23 13:16:52 +0100

    chore connect app redux store to dev tools

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -6,12 +6,16 @@ import { routerMiddleware as createRouterMiddleware } from 'react-router-redux';
 import rootReducer from './rootReducer';
 import rootSaga from './rootSaga';
 
+const DEBUG = process.env.NODE_ENV !== 'production';
+
 export default () => {
   const initialState = fromJS({});
   const sagaMiddleware = createSagaMiddleware();
   const routerMiddleware = createRouterMiddleware(browserHistory);
   const enhancer = compose(
-    applyMiddleware(sagaMiddleware, routerMiddleware));
+    applyMiddleware(sagaMiddleware, routerMiddleware),
+    (DEBUG && window.devToolsExtension) ? window.devToolsExtension() : f => f
+  );
   const store = createStore(rootReducer, initialState, enhancer);
   sagaMiddleware.run(rootSaga);
 
