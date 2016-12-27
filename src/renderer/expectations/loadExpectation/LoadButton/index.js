da74f364666127f0aa1d937460b5f4ebe3bb38ca
Bartosz Adamczyk <born2perform1@gmail.com>
2016-12-27 01:22:55 +0100

    refactor rename wrong action name

--- src/renderer/expectations/loadExpectation/LoadButton/index.js
+++ src/renderer/expectations/loadExpectation/LoadButton/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { init } from '../actions';
 
-const LoadButton = ({ unload, serverId, instanceId }) => (
-  <button onClick={() => unload(serverId, instanceId)}>{'Load'}</button>
+const LoadButton = ({ load, serverId, instanceId }) => (
+  <button onClick={() => load(serverId, instanceId)}>{'Load'}</button>
 );
 
 LoadButton.propTypes = {
-  unload: React.PropTypes.func.isRequired,
+  load: React.PropTypes.func.isRequired,
   serverId: React.PropTypes.string.isRequired,
   instanceId: React.PropTypes.string.isRequired
 };
 
 const mapDispatchToProps = {
-  unload: init
+  load: init
 };
 
 export default connect(null, mapDispatchToProps)(LoadButton);
