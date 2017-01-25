fb80c725d2fc4fa70928d76a60a05fe8e9b53113
Bartosz Adamczyk <born2perform1@gmail.com>
2017-01-25 17:17:13 +0100

    refactor improve naming

--- src/renderer/servers/startServer/saga.js
+++ src/renderer/servers/startServer/saga.js
@@ -6,14 +6,14 @@ import {
 import { remote } from 'electron';
 import { INIT } from './actions';
 import { start } from '../actions';
-import electronRemoteFn from '../../helpers/electron';
+import prepareForEffect from '../../utils/redux-saga';
 
 export default function* agent() {
   // eslint-disable-next-line no-constant-condition
   while (true) {
     const { id } = yield take(INIT);
     const remoteStart = remote.require('./dist/main/servers').default.start;
-    yield call(electronRemoteFn(remoteStart), id);
+    yield call(prepareForEffect(remoteStart), id);
     yield put(start(id));
   }
 }
--- src/renderer/servers/stopServer/saga.js
+++ src/renderer/servers/stopServer/saga.js
@@ -2,14 +2,14 @@ import { take, call, put } from 'redux-saga/effects';
 import { remote } from 'electron';
 import { stop } from '../actions';
 import { INIT } from './actions';
-import electronRemoteFn from '../../helpers/electron';
+import prepareForEffect from '../../utils/redux-saga';
 
 export default function* agent() {
   // eslint-disable-next-line no-constant-condition
   while (true) {
     const { id } = yield take(INIT);
     const remoteStop = remote.require('./dist/main/servers').default.stop;
-    yield call(electronRemoteFn(remoteStop), id);
+    yield call(prepareForEffect(remoteStop), id);
     yield put(stop(id));
   }
 }
