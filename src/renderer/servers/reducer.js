2a2cad7eec6a3cd7377730f538324196155c6ab2
Bartosz Adamczyk <born2perform1@gmail.com>
2017-02-01 11:13:16 +0100

    refactor rewrite STOP, ADD reducers

--- src/renderer/servers/reducer.js
+++ src/renderer/servers/reducer.js
@@ -27,30 +27,28 @@ const addId = R.curry(
   (queueId, queues) => queues.push(queueId)
 );
 
-const addQueue = R.curry(
+const updateServerQueues = R.curry(
   (serverId, updater, currentState) => currentState.updateIn(['itemsById', serverId, 'queues'], updater)
 );
 
 const select = R.curry((id, currentState) => currentState.set('selected', id));
 const updateRunning = R.curry((updater, currentState) => currentState.set('running', updater));
 const addToRunning = R.curry((id, runningServers) => runningServers.add(id));
+const removeFromRunning = R.curry((id, runningServers) => runningServers.delete(id));
+const addServer = R.curry((id, server, currentState) => currentState.setIn(['itemsById', id], server));
+const createServer = R.pipe(
+  R.pick(['name', 'port', 'id', 'serverType']),
+  R.over(
+    R.lens(R.prop('serverType'), R.assoc('type')),
+    R.identity
+  ),
+  R.construct(Server)
+);
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD: {
-      return R.invoker(2, 'set')('itemsById', R.pipe(
-        R.invoker(1, 'get')('itemsById'),
-        R.invoker(2, 'set')(action.id,
-          R.pipe(
-            R.pick(['name', 'port', 'id', 'serverType']),
-            R.over(
-              R.lens(R.prop('serverType'), R.assoc('type')),
-              R.identity
-            ),
-            R.construct(Server)
-          )(action)
-        ),
-      )(state), state);
+      return addServer(action.id, createServer(action), state);
     }
     case SELECT: {
       return select(action.id, state);
@@ -59,17 +57,12 @@ export default (state = initialState, action) => {
       return updateRunning(addToRunning(action.id), state);
     }
     case STOP: {
-      return R.invoker(2, 'set')('running',
-        R.pipe(
-          R.invoker(1, 'get')('running'),
-          R.invoker(1, 'delete')(action.id),
-        )(state),
-        state);
+      return updateRunning(removeFromRunning(action.id), state);
     }
     case ADD_QUEUE: {
       const { serverId, queueId } = action;
 
-      return addQueue(serverId, addId(queueId))(state);
+      return updateServerQueues(serverId, addId(queueId), state);
     }
     default: {
       return state;
