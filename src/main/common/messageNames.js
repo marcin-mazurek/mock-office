2e687f28f8fb4f3c349b1b123d6a37636e24a105
Bartosz Adamczyk <born2perform1@gmail.com>
2017-02-05 13:50:51 +0100

    chore start real versioning

---
+++ src/main/common/messageNames.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const REMOVE_RESPONSE_AFTER_USE = 'SERVER_START';
---
+++ src/renderer/responses/pickFile/FilePicker/FilePicker.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { init as initAddQueueFromFile } from '../actions';
+
+const mapDispatchToProps = {
+  initAddQueueFromFile
+};
+
+class FilePicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    this.props.initAddQueueFromFile(this.fileNode.files);
+  }
+
+  render() {
+    const { handleChange } = this;
+
+    return (
+      <input
+        type="file"
+        ref={(node) => { this.fileNode = node; }}
+        onChange={handleChange}
+      />
+    );
+  }
+}
+
+FilePicker.propTypes = {
+  initAddQueueFromFile: React.PropTypes.func.isRequired
+};
+
+export default connect(null, mapDispatchToProps)(FilePicker);
