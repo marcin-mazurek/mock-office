3d02f0c4a2fe6dd98e1ee8d59c98bbae6cea38b7
Bartosz Adamczyk <born2perform1@gmail.com>
2017-02-09 12:34:48 +0100

    refactor remove unused code

--- src/main/persistent/index.js
+++
@@ -1,9 +0,0 @@
-import uniqueId from 'node-unique';
-import fs from 'fs';
-
-export default path => JSON.parse(fs.readFileSync(path))
-  .map((mockFromFile) => {
-    const mock = mockFromFile;
-    mock.id = uniqueId();
-    return mock;
-  });
