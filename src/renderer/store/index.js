74d9a41728b5183e31c5f8def66de2b6eff4336e
Bartosz Adamczyk <born2perform1@gmail.com>
2017-02-23 00:13:14 +0100

    Revert "refactor remove redux-saga"
    This reverts commit 4fb4b0ec7efdfb714c8312a98cee66f79e1aae3d.

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,3 +1,4 @@
+import createSagaMiddleware from 'redux-saga';
 import { fromJS } from 'immutable';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { hashHistory } from 'react-router';
@@ -5,6 +6,7 @@ import { routerMiddleware as createRouterMiddleware } from 'react-router-redux';
 import { createEpicMiddleware } from 'redux-observable';
 import 'rxjs';
 import rootReducer from './rootReducer';
+import rootSaga from './rootSaga';
 import rootEpic from './rootEpic';
 
 const epicMiddleware = createEpicMiddleware(rootEpic);
@@ -12,11 +14,13 @@ const epicMiddleware = createEpicMiddleware(rootEpic);
 const DEBUG = process.env.NODE_ENV !== 'production';
 
 const initialState = fromJS({});
+const sagaMiddleware = createSagaMiddleware();
 const routerMiddleware = createRouterMiddleware(hashHistory);
 const enhancer = compose(
-  applyMiddleware(routerMiddleware, epicMiddleware),
+  applyMiddleware(sagaMiddleware, routerMiddleware, epicMiddleware),
   (DEBUG && window.devToolsExtension) ? window.devToolsExtension() : f => f
 );
 const store = createStore(rootReducer, initialState, enhancer);
+sagaMiddleware.run(rootSaga);
 
 export default store;
