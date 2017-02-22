102d9637b10c9d810154de3085de1afe2cbb3938
Bartosz Adamczyk <born2perform1@gmail.com>
2017-02-23 00:37:41 +0100

    refactor App
    Move some app logic not related with react outside component

--- src/renderer/app/App.js
+++ src/renderer/app/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { remote } from 'electron';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
@@ -7,11 +6,6 @@ import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material';
 import store from '../store';
 import routes from '../routes';
-import removeTaskAfterUse from '../tasks/removeTaskAfterUse';
-import syncServers from './syncServers';
-
-removeTaskAfterUse(store);
-syncServers(remote, store);
 
 const history = syncHistoryWithStore(hashHistory, store, {
   selectLocationState(state) {
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { remote } from 'electron';
 import App from './app/App';
+import removeTaskAfterUse from './tasks/removeTaskAfterUse';
+import syncServers from './app/syncServers';
+import store from './store';
+
+removeTaskAfterUse(store);
+syncServers(remote, store);
 
 render(<App />, document.querySelector('.js-main'));
