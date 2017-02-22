039f0baba5c165f80845e46bb053524046fa9e29
Bartosz Adamczyk <born2perform1@gmail.com>
2017-02-22 23:48:08 +0100

    refactor simplify

--- src/renderer/tasks/reducer.js
+++ src/renderer/tasks/reducer.js
@@ -6,7 +6,6 @@ import { ADD } from './addTask/actions';
 const initialState = new Map();
 
 const addTask = R.invoker(2, 'set');
-const extendWithId = id => R.assoc('id', id);
 const removeTask = R.invoker(1, 'remove');
 
 export default (state = initialState, action) => {
@@ -14,7 +13,7 @@ export default (state = initialState, action) => {
     case ADD: {
       const { taskId, taskPayload } = action;
 
-      return addTask(taskId, extendWithId(taskId)({ taskPayload }))(state);
+      return addTask(taskId, ({ taskPayload, id: taskId }))(state);
     }
     case REMOVE: {
       const { taskId } = action;
