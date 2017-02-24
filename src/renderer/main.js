45bb07c550e62dcec9dbab728060964a36cf01ef
Bartosz Adamczyk <born2perform1@gmail.com>
2017-02-24 13:29:16 +0100

    fix provide proper arguments to syncState

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { remote } from 'electron';
 import App from './app/App';
 import syncTasks from './tasks/syncTasks';
 import syncState from './app/syncState';
 import store from './store';
 
 syncTasks(store);
-syncState(remote, store);
+syncState(store);
 
 render(<App />, document.querySelector('.js-main'));
