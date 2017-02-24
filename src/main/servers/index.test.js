96d71e45a86ccbb76befd4cc4467c4e167367afc
Bartosz Adamczyk <born2perform1@gmail.com>
2017-02-24 16:53:47 +0100

    test dont start/stop servers in tests

--- src/main/servers/index.test.js
+++ src/main/servers/index.test.js
@@ -1,3 +1,4 @@
+import sinon from 'sinon';
 import serversHub from './index';
 import HttpServer from './httpServer';
 import WsServer from './wsServer';
@@ -30,24 +31,86 @@ test('serversHub.add should throw error if we provide unknown server type', () =
   ).toThrow();
 });
 
-test('serversHub.start should start server if it is not running', () => {
-  const { serverId } = serversHub.add('server name', 3000, 'http', false);
-  expect(serversHub.start(serverId)).toBeInstanceOf(Promise);
+test('serversHub.start should call server start if it is not running', () => {
+  const stubFn = sinon.spy();
+  const serverId = 'some-id';
+
+  serversHub.servers = [
+    {
+      id: serverId,
+      isLive() {
+        return false;
+      },
+      start(cb) {
+        cb();
+        stubFn();
+      }
+    }
+  ];
+
+  serversHub.start(serverId);
+  expect(stubFn.called).toBeTruthy();
 });
 
-test('serversHub.start should throw when it doesn\'t find server', () => {
-  expect(serversHub.start('some unknown server Id')).toThrow();
+test('serversHub.start should not call server start if it is running', () => {
+  const stubFn = sinon.spy();
+  const serverId = 'some-id';
+
+  serversHub.servers = [
+    {
+      id: serverId,
+      isLive() {
+        return true;
+      },
+      start(cb) {
+        cb();
+        stubFn();
+      }
+    }
+  ];
+
+  serversHub.start(serverId);
+  expect(stubFn.called).toBeFalsy();
 });
 
-test('serversHub.stop should return promise that server will stop, if it find server', () => {
-  const { serverId } = serversHub.add('server name', 3000, 'http', false);
-  expect(serversHub.stop(serverId)).toBeInstanceOf(Promise);
+test('serversHub.stop should call server stop if it is running', () => {
+  const stubFn = sinon.spy();
+  const serverId = 'some-id';
+
+  serversHub.servers = [
+    {
+      id: serverId,
+      isLive() {
+        return true;
+      },
+      stop(cb) {
+        cb();
+        stubFn();
+      }
+    }
+  ];
+
+  serversHub.stop(serverId);
+  expect(stubFn.called).toBeTruthy();
 });
 
-test('serversHub.stop should throw when it doesn\'t find server', () => {
-  expect(() => { serversHub.stop('some unknown server Id'); }).toThrow();
-});
+test('serversHub.stop should not call server stop if it is not running', () => {
+  const stubFn = sinon.spy();
+  const serverId = 'some-id';
 
-afterEach(() => {
-  serversHub.getAll().forEach((server) => { serversHub.stop(server.id); });
+  serversHub.servers = [
+    {
+      id: serverId,
+      isLive() {
+        return false;
+      },
+      stop(cb) {
+        cb();
+        stubFn();
+      }
+    }
+  ];
+
+  serversHub.stop(serverId);
+  expect(stubFn.called).toBeFalsy();
 });
