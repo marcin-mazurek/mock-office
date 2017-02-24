c273d86b686d263f1ab2f14df9dd0c134b9e1ee7
Bartosz Adamczyk <born2perform1@gmail.com>
2017-02-24 12:50:21 +0100

    refactor
    flatten file structure, rename

--- src/renderer/tasks/syncTasks.js
+++ src/renderer/tasks/syncTasks.js
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs';
 import { ipcRenderer } from 'electron';
-import { TASK_REMOVED } from '../../../main/common/messageNames';
-import { remove } from '../removeTask/actions';
+import { TASK_REMOVED } from '../../main/common/messageNames';
+import { remove } from './removeTask/actions';
 
 export default (store) => {
   Observable.bindCallback(
