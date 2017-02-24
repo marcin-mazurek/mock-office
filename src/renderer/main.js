16dc497aeb3c4927d5025c7f3260be4648c66e03
Bartosz Adamczyk <born2perform1@gmail.com>
2017-02-24 12:55:18 +0100

    fix provide store to syncTasks

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,10 +3,10 @@ import { render } from 'react-dom';
 import { remote } from 'electron';
 import App from './app/App';
 import syncTasks from './tasks/syncTasks';
-import syncServers from './app/syncServers';
+import syncServers from './app/syncState';
 import store from './store';
 
-syncTasks();
+syncTasks(store);
 syncServers(remote, store);
 
 render(<App />, document.querySelector('.js-main'));
