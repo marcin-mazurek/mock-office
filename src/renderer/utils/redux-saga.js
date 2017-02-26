557a038bd038497b1a933da7b630c005063461c5
Bartosz Adamczyk <born2perform1@gmail.com>
2017-02-26 20:48:14 +0100

    refactor remove unused files
    We dont need utils for redux-saga since we started using redux-observable

--- src/renderer/utils/redux-saga.js
+++
@@ -1,5 +0,0 @@
-export default (proxiedFn) => {
-  // eslint-disable-next-line no-param-reassign
-  proxiedFn.toString = Object.prototype.toString;
-  return proxiedFn;
-};
--- src/renderer/utils/redux-saga.test.js
+++
@@ -1,9 +0,0 @@
-import prepareForCall from './redux-saga';
-
-test('prepareForCall should prepare method to be used by redux-saga call task', () => {
-  const someFn = () => {};
-  const proxiedFn = new Proxy(someFn, {});
-
-  expect(proxiedFn.toString).toThrow();
-  expect(prepareForCall(proxiedFn)).not.toThrow();
-});
