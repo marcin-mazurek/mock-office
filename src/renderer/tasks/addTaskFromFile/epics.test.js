c207bf6ab2acb4c61aab7b28b16d854814b5bca9
Bartosz Adamczyk <born2perform1@gmail.com>
2017-02-28 09:34:07 +0100

    test cover addTaskFromFile epic

---
+++ src/renderer/tasks/addTaskFromFile/epics.test.js
@@ -0,0 +1,38 @@
+import { Map, Set } from 'immutable';
+import configureMockStore from 'redux-mock-store';
+import 'rxjs';
+import { createEpicMiddleware } from 'redux-observable';
+import addTaskFromFileEpic from './epics';
+import { init } from './actions';
+
+let store;
+const epicMiddleware = createEpicMiddleware(addTaskFromFileEpic);
+const mockStore = configureMockStore([epicMiddleware]);
+
+afterEach(() => {
+  store.clearActions();
+});
+
+test('addTaskFromFileEpic', () => {
+  store = mockStore(new Map({
+    servers: new Map({
+      itemsById: new Map({
+        'server id': {
+          type: 'server type',
+          name: 'server name',
+          port: 3000,
+          id: 'server id',
+          queue: 'queue'
+        }
+      }),
+      selected: 'server id',
+      running: new Set()
+    }),
+    tasks: new Map()
+  }));
+
+  store.dispatch(init([
+    new Blob(['{"payload": "task payload"}'])
+  ]));
+  expect(store.getActions()).toMatchSnapshot();
+});
