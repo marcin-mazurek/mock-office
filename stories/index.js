8218ad22f63f8088fc1cb39342e79cfe8205f949
Bartosz Adamczyk <born2perform1@gmail.com>
2017-03-01 11:04:49 +0100

    chore fix navbar servers story

--- stories/index.js
+++ stories/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { Map, Record } from 'immutable';
 import '../src/renderer/styles/main.scss';
-
 import NavBar from '../src/renderer/navbar/NavBar';
+
+const mockStore = configureStore();
+
+const state = new Map({
+  servers: new Map({
+    itemsById: new Map({
+      'some-id': new Record({
+        id: '',
+        name: 'New Server',
+        port: null,
+        type: '',
+        queue: ''
+      })({
+        id: 'some-id'
+      })
+    }),
+    selected: null,
+    running: new Set()
+  })
+});
 
 storiesOf('NavBar', module)
   .add('default', () => (
-    <NavBar />
+    <Provider store={mockStore(state)}>
+      <NavBar />
+    </Provider>
   ));
