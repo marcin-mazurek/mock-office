f15812ff5c53cccf3711fecc70ff908da959c26a
Bartosz Adamczyk <born2perform1@gmail.com>
2017-03-01 11:04:42 +0100

    refactor flatten components structure

--- src/renderer/servers/inspectServer/Server.js
+++ src/renderer/servers/inspectServer/Server.js
@@ -11,37 +11,35 @@ export const ServerInspect = ({ running, serverDetails }) => {
   const { name, port, type, queue, id } = serverDetails;
 
   return (
-    <div className="server">
-      <div className="server-inspect">
-        <header className="server-inspect__header">
-          { running ? <StopButtonConnect /> : <StartButtonConnect /> } {name}
-        </header>
-        <main className="server-inspect-main server-inspect__main">
-          <div className="server-inspect-details">
-            <div className="server-inspect-details__row">
-              <div className="server-inspect-details__cell">
-                Port: {port}
-              </div>
-              <div className="server-inspect-details__cell">
-                Type: {type}
-              </div>
+    <div className="server-inspect">
+      <header className="server-inspect__header">
+        { running ? <StopButtonConnect /> : <StartButtonConnect /> } {name}
+      </header>
+      <main className="server-inspect-main server-inspect__main">
+        <div className="server-inspect-details">
+          <div className="server-inspect-details__row">
+            <div className="server-inspect-details__cell">
+              Port: {port}
             </div>
-            <div className="server-inspect-details__row">
-              <div className="server-inspect-details__cell">
-                <Link to="/add-task">Add task</Link>
-              </div>
-            </div>
-            <div className="server-inspect-details__row">
-              <div className="server-inspect-details__cell">
-                Add from file: <FilePickerConnect />
-              </div>
+            <div className="server-inspect-details__cell">
+              Type: {type}
             </div>
           </div>
-          <div className="server-inspect-queue">
-            <Queue id={queue} serverId={id} />
+          <div className="server-inspect-details__row">
+            <div className="server-inspect-details__cell">
+              <Link to="/add-task">Add task</Link>
+            </div>
           </div>
-        </main>
-      </div>
+          <div className="server-inspect-details__row">
+            <div className="server-inspect-details__cell">
+              Add from file: <FilePickerConnect />
+            </div>
+          </div>
+        </div>
+        <div className="server-inspect-queue">
+          <Queue id={queue} serverId={id} />
+        </div>
+      </main>
     </div>
   );
 };
@@ -51,23 +49,22 @@ ServerInspect.propTypes = {
   serverDetails: React.PropTypes.shape({})
 };
 
-const mapStateToProps = state => ({
-  running: isRunning(state),
-  serverDetails: getSelectedServerDetails(state)
-});
-
-const ServerInspectConnect = connect(mapStateToProps)(ServerInspect);
-
-export const Server = ({ selected }) => (
-  selected ? <ServerInspectConnect /> : <div>Server is not selected</div>
+export const Server = ({ selected, running, serverDetails }) => (
+  selected
+    ? <ServerInspect running={running} serverDetails={serverDetails} />
+    : <div>Server is not selected</div>
 );
 
 Server.propTypes = {
-  selected: React.PropTypes.string
+  selected: React.PropTypes.string,
+  running: React.PropTypes.bool.isRequired,
+  serverDetails: React.PropTypes.shape({})
 };
 
 const serverMapStateToProps = state => ({
-  selected: getSelected(state)
+  selected: getSelected(state),
+  running: isRunning(state),
+  serverDetails: getSelectedServerDetails(state)
 });
 
 export default connect(serverMapStateToProps)(Server);
