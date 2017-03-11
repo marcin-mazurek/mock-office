9d72b55bc801d4fbe3959ad6e2fceaa1cc02f59b
Bartosz Adamczyk <born2perform1@gmail.com>
2017-03-11 10:06:14 +0100

    refactor move task management to Queue class

--- src/main/queues/Queue.js
+++ src/main/queues/Queue.js
@@ -1,3 +1,203 @@
-class Queue {
+import unique from 'node-unique';
+import Task from 'fun-task';
+import { EventEmitter } from 'events';
+import R from 'ramda';
+import deepEqual from 'deep-equal';
+import extractSubTree from './extractSubtree';
+import { events as queuesEvents } from './index';
 
+export const events = {
+  TASK_REMOVED: 'TASK_REMOVED',
+  TASK_RUN: 'TASK_RUN',
+  TASK_ADDED: 'TASK_ADDED'
+};
+
+export default class Queue {
+  constructor(args) {
+    this.id = args.id;
+    this.serverId = args.serverId;
+    this.queuesEmitter = args.emitter;
+    this.tasks = [];
+    this.emitter = new EventEmitter();
+    this.runReadyTasks = this.runReadyTasks.bind(this);
+    this.emitter.on(events.TASK_REMOVED, this.runReadyTasks);
+    this.emitter.on(events.TASK_RUN, this.runReadyTasks);
+    this.emitter.on(events.TASK_ADDED, this.runReadyTasks);
+  }
+
+  createTask(task) {
+    let job;
+    const t = task;
+
+    if (t.interval) {
+      job = Task.create(() => {
+        t.running = true;
+        const intervalId = setInterval(() =>
+          this.tunnel({
+            taskPayload: t.taskPayload,
+            headers: t.headers
+          }), t.interval
+        );
+
+        return () => {
+          t.running = false;
+          clearInterval(intervalId);
+        };
+      });
+    } else if (t.delay) {
+      job = Task.create((onSuccess) => {
+        t.running = true;
+        const timeoutId = setTimeout(() => {
+          this.tunnel({
+            taskPayload: t.taskPayload,
+            headers: t.headers
+          });
+          onSuccess();
+        }, t.delay);
+
+        return () => {
+          t.running = false;
+          clearTimeout(timeoutId);
+        };
+      });
+    } else {
+      job = Task.create((onSuccess) => {
+        t.running = true;
+        this.tunnel({
+          taskPayload: t.taskPayload,
+          headers: t.headers
+        });
+        onSuccess();
+
+        return () => {
+          t.running = false;
+        };
+      });
+    }
+
+    return Object.assign(t, {
+      id: unique(),
+      job
+    });
+  }
+
+  addTask(taskConfig) {
+    const task = this.createTask(taskConfig);
+    this.tasks.push(task);
+    this.emitter.emit(events.TASK_ADDED);
+
+    return task.id;
+  }
+
+  runTask(taskId) {
+    const task = this.tasks.find(exp => exp.id === taskId);
+
+    task.cancel = task.job.run(() => {
+      task.running = false;
+
+      if (task.reuse === 'infinite') {
+        R.identity(); // do nothing
+      } else if (task.reuse === 'fixed') {
+        task.quantity -= 1;
+
+        if (task.quantity === 0) {
+          this.removeTask(task.id);
+          this.queuesEmitter.emit(
+            queuesEvents.TASK_REMOVED_AFTER_USE,
+            { queueId: this.id, taskId }
+          );
+        }
+      } else {
+        this.removeTask(task.id);
+        this.queuesEmitter.emit(queuesEvents.TASK_REMOVED_AFTER_USE, { queueId: this.id, taskId });
+      }
+    });
+  }
+
+  removeTask(taskId) {
+    const taskIndex = this.tasks.findIndex(task => task.id === taskId);
+    const task = this.tasks[taskIndex];
+
+    if (task.cancel) {
+      task.cancel();
+      task.cancel = undefined;
+    }
+
+    this.tasks.splice(taskIndex, 1);
+    this.emitter.emit(events.TASK_REMOVED);
+  }
+
+  runReadyTasks(requirements, cb) {
+    if (this.tunnel) {
+      // collect phase
+      let blockingTaskFound = false;
+      const notBlockedTasks = R.takeWhile(
+        (task) => {
+          if (blockingTaskFound) {
+            return false;
+          }
+
+          blockingTaskFound = task.behaviours;
+          return true;
+        },
+        this.tasks
+      );
+
+      if (notBlockedTasks.length === 0) {
+        if (cb) {
+          cb();
+        }
+
+        return;
+      }
+
+      const runnableTasks = notBlockedTasks.filter((task) => {
+        if (task.running) {
+          return false;
+        }
+
+        // do not need
+        if (!task.requirements) {
+          return true;
+        }
+
+        // should fulfill requirements
+        return deepEqual(task.requirements, extractSubTree(requirements, task.requirements, {}));
+      });
+
+      if (runnableTasks.length === 0) {
+        if (cb) {
+          cb();
+        }
+
+        return;
+      }
+
+      const firstRunnableTask = R.head(runnableTasks);
+
+      if (cb) {
+        firstRunnableTask.job.map(cb);
+      }
+
+      this.runTask(firstRunnableTask.id);
+      this.emitter.emit(events.TASK_RUN);
+    }
+  }
+
+  cancelPendingTasks() {
+    this.tasks.forEach((task) => {
+      if (task.cancel) {
+        task.cancel();
+      }
+    });
+  }
+
+  openTunnel(tunnel) {
+    this.tunnel = tunnel;
+  }
+
+  closeTunnel() {
+    this.cancelPendingTasks();
+    this.tunnel = undefined;
+  }
 }
--- src/main/queues/index.js
+++ src/main/queues/index.js
@@ -1,81 +1,19 @@
 import unique from 'node-unique';
-import deepEqual from 'deep-equal';
-import R from 'ramda';
-import Task from 'fun-task';
 import { EventEmitter } from 'events';
-import extractSubTree from './extractSubtree';
+import Queue from './Queue';
 
 export const events = {
-  TASK_REMOVED: 'TASK_REMOVED',
-  TASK_RUN: 'TASK_RUN',
-  TASK_ADDED: 'TASK_ADDED',
   TASK_REMOVED_AFTER_USE: 'TASK_REMOVED_AFTER_USE'
 };
 
 export const emitter = new EventEmitter();
 const queues = [];
 
-const createQueue = serverId => ({
+const createQueue = serverId => new Queue({
   id: unique(),
   serverId,
-  tasks: []
+  emitter
 });
-
-const createTask = (queue, task) => {
-  let job;
-  const t = task;
-
-  if (t.interval) {
-    job = Task.create(() => {
-      t.running = true;
-      const intervalId = setInterval(() =>
-        queue.tunnel({
-          taskPayload: t.taskPayload,
-          headers: t.headers
-        }), t.interval
-      );
-
-      return () => {
-        t.running = false;
-        clearInterval(intervalId);
-      };
-    });
-  } else if (t.delay) {
-    job = Task.create((onSuccess) => {
-      t.running = true;
-      const timeoutId = setTimeout(() => {
-        queue.tunnel({
-          taskPayload: t.taskPayload,
-          headers: t.headers
-        });
-        onSuccess();
-      }, t.delay);
-
-      return () => {
-        t.running = false;
-        clearTimeout(timeoutId);
-      };
-    });
-  } else {
-    job = Task.create((onSuccess) => {
-      t.running = true;
-      queue.tunnel({
-        taskPayload: t.taskPayload,
-        headers: t.headers
-      });
-      onSuccess();
-
-      return () => {
-        t.running = false;
-      };
-    });
-  }
-
-  return Object.assign(t, {
-    id: unique(),
-    job
-  });
-};
 
 const addQueue = (serverId) => {
   const q = createQueue(serverId);
@@ -87,137 +25,28 @@ const addQueue = (serverId) => {
 const getQueue = id => queues.find(q => q.id === id);
 
 const removeTask = (queueId, taskId) => {
-  const queue = getQueue(queueId);
-  const taskIndex = queue.tasks.findIndex(task => task.id === taskId);
-  const task = queue.tasks[taskIndex];
-
-  if (task.cancel) {
-    task.cancel();
-    task.cancel = undefined;
-  }
-
-  queue.tasks.splice(taskIndex, 1);
+  getQueue(queueId).removeTask(taskId);
   emitter.emit(events.TASK_REMOVED, queueId);
 };
 
-const runTask = (queueId, taskId) => {
-  const queue = queues.find(q => q.id === queueId);
-  const task = queue.tasks.find(exp => exp.id === taskId);
-
-  task.cancel = task.job.run(() => {
-    task.running = false;
-
-    if (task.reuse === 'infinite') {
-      R.identity(); // do nothing
-    } else if (task.reuse === 'fixed') {
-      task.quantity -= 1;
-
-      if (task.quantity === 0) {
-        removeTask(queueId, task.id);
-        emitter.emit(events.TASK_REMOVED_AFTER_USE, { queueId, taskId });
-      }
-    } else {
-      removeTask(queueId, task.id);
-      emitter.emit(events.TASK_REMOVED_AFTER_USE, { queueId, taskId });
-    }
-  });
-};
-
 const runReadyTasks = (queueId, requirements, cb) => {
-  const queue = getQueue(queueId);
-
-  if (queue.tunnel) {
-    // collect phase
-    let blockingTaskFound = false;
-    const notBlockedTasks = R.takeWhile(
-      (task) => {
-        if (blockingTaskFound) {
-          return false;
-        }
-
-        blockingTaskFound = task.behaviours;
-        return true;
-      },
-      queue.tasks
-    );
-
-    if (notBlockedTasks.length === 0) {
-      if (cb) {
-        cb();
-      }
-
-      return;
-    }
-
-    const runnableTasks = notBlockedTasks.filter((task) => {
-      if (task.running) {
-        return false;
-      }
-
-      // do not need
-      if (!task.requirements) {
-        return true;
-      }
-
-      // should fulfill requirements
-      return deepEqual(task.requirements, extractSubTree(requirements, task.requirements, {}));
-    });
-
-    if (runnableTasks.length === 0) {
-      if (cb) {
-        cb();
-      }
-
-      return;
-    }
-
-    const firstRunnableTask = R.head(runnableTasks);
-
-    if (cb) {
-      firstRunnableTask.job.map(cb);
-    }
-
-    runTask(queueId, firstRunnableTask.id);
-    emitter.emit(events.TASK_RUN, queueId);
-  }
+  getQueue(queueId).runReadyTasks(requirements, cb);
 };
 
-const addTask = (queueId, task) => {
-  const queue = getQueue(queueId);
-  const res = createTask(queue, task);
-  queue.tasks.push(res);
-
-  emitter.emit(events.TASK_ADDED, queueId);
-
-  return res.id;
-};
-
+const addTask = (queueId, task) => getQueue(queueId).addTask(task);
 const getAll = () => queues;
 
 const cancelPendingTasks = (queueId) => {
-  const queue = getQueue(queueId);
-
-  queue.tasks.forEach((task) => {
-    if (task.cancel) {
-      task.cancel();
-    }
-  });
+  getQueue(queueId).cancelPendingTasks();
 };
 
 const openTunnel = (queueId, tunnel) => {
-  const queue = getQueue(queueId);
-  queue.tunnel = tunnel;
+  getQueue(queueId).openTunnel(tunnel);
 };
 
 const closeTunnel = (queueId) => {
-  const queue = getQueue(queueId);
-  cancelPendingTasks(queueId);
-  queue.tunnel = undefined;
+  getQueue(queueId).closeTunnel();
 };
-
-emitter.on(events.TASK_REMOVED, queueId => runReadyTasks(queueId));
-emitter.on(events.TASK_RUN, queueId => runReadyTasks(queueId));
-emitter.on(events.TASK_ADDED, queueId => runReadyTasks(queueId));
 
 export default {
   addQueue,
@@ -226,7 +55,6 @@ export default {
   runReadyTasks,
   getAll,
   removeTask,
-  runTask,
   cancelPendingTasks,
   openTunnel,
   closeTunnel
