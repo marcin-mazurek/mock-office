36332a38630051f548cbc8b8513e95143b075951
Bartosz Adamczyk <born2perform1@gmail.com>
2017-03-12 23:21:32 +0100

    refactor queues reducer

--- src/renderer/queues/reducer.js
+++ src/renderer/queues/reducer.js
@@ -1,5 +1,4 @@
 import { Map, Record, List } from 'immutable';
-import R from 'ramda';
 import { ADD as ADD_SERVER } from '../servers/actions';
 import { REMOVE as REMOVE_TASK } from '../tasks/removeTask/actions';
 import { ADD as ADD_TASK } from '../tasks/addTask/actions';
@@ -7,39 +6,23 @@ import { ADD as ADD_TASK } from '../tasks/addTask/actions';
 const initialState = new Map();
 
 const Queue = new Record({
-  id: '',
   tasks: new List()
 });
-
-const addTask = R.invoker(1, 'push');
-const removeTask = R.curry(
-  (task, tasks) => tasks.delete(
-    tasks.findIndex(res => res === task),
-    1)
-);
-const updateTasks = R.curry(
-  (serverId, updater, currentState) =>
-    currentState.updateIn([serverId, 'tasks'], updater)
-);
-const constructQueue = serverId => new Queue({ id: serverId });
-const addToQueues = R.invoker(2, 'set');
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_SERVER: {
-      const { id: serverId } = action;
-
-      return addToQueues(serverId, constructQueue(serverId))(state);
+      const queue = new Queue();
+      return state.set(action.id, queue);
     }
     case ADD_TASK: {
-      const { serverId, taskId } = action;
-
-      return updateTasks(serverId, addTask(taskId))(state);
+      return state.updateIn([action.serverId, 'tasks'], tasks => tasks.push(action.taskId));
     }
     case REMOVE_TASK: {
-      const { serverId, taskId } = action;
-
-      return updateTasks(serverId, removeTask(taskId))(state);
+      return state.updateIn([action.serverId, 'tasks'], (tasks) => {
+        const taskIndex = tasks.findIndex(taskId => taskId === action.taskId);
+        return tasks.delete(taskIndex, 1);
+      });
     }
     default: {
       return state;
--- src/renderer/queues/reducer.test.js
+++ src/renderer/queues/reducer.test.js
@@ -6,56 +6,51 @@ import {
 import { REMOVE as REMOVE_TASK } from '../tasks/removeTask/actions';
 import { ADD as ADD_TASK } from '../tasks/addTask/actions';
 
-test(`${ADD_SERVER} reducer`, () => {
-  const state = reducer(fromJS({}),
-    { type: ADD_SERVER, id: 'AVoPUwib8A1waDvCQ0id6T1QcfLxWQ==' });
+describe('queues reducer', () => {
+  test(`${ADD_SERVER} action`, () => {
+    const state = reducer(fromJS({}),
+      { type: ADD_SERVER, id: 'AVoPUwib8A1waDvCQ0id6T1QcfLxWQ==' });
 
-  expect(state.toJS()).toEqual({
-    'AVoPUwib8A1waDvCQ0id6T1QcfLxWQ==': {
-      id: 'AVoPUwib8A1waDvCQ0id6T1QcfLxWQ==',
-      tasks: []
-    }
-  });
-});
-
-test(`${ADD_TASK} reducer`, () => {
-  const state = reducer(fromJS({
-    'AVn5d/fFT13LWVVYQzKEVy1VkdW4vQ==': {
-      id: 'AVn5d/fFT13LWVVYQzKEVy1VkdW4vQ==',
-      requirements: { url: '/some-url-3' },
-      tasks: []
-    }
-  }), {
-    type: ADD_TASK,
-    serverId: 'AVn5d/fFT13LWVVYQzKEVy1VkdW4vQ==',
-    taskId: 'AVn5d/fWdqw6Cki7TYu1mFutjHiDEQ=='
+    expect(state.toJS()).toEqual({
+      'AVoPUwib8A1waDvCQ0id6T1QcfLxWQ==': {
+        tasks: []
+      }
+    });
   });
 
-  expect(state.toJS()).toEqual({
-    'AVn5d/fFT13LWVVYQzKEVy1VkdW4vQ==': {
-      id: 'AVn5d/fFT13LWVVYQzKEVy1VkdW4vQ==',
-      requirements: { url: '/some-url-3' },
-      tasks: ['AVn5d/fWdqw6Cki7TYu1mFutjHiDEQ==']
-    }
-  });
-});
+  test(`${ADD_TASK} action`, () => {
+    const state = reducer(fromJS({
+      'AVn5d/fFT13LWVVYQzKEVy1VkdW4vQ==': {
+        tasks: []
+      }
+    }), {
+      type: ADD_TASK,
+      serverId: 'AVn5d/fFT13LWVVYQzKEVy1VkdW4vQ==',
+      taskId: 'AVn5d/fWdqw6Cki7TYu1mFutjHiDEQ=='
+    });
 
-test(`${REMOVE_TASK} reducer`, () => {
-  const state = reducer(fromJS({
-    'AVn5tu8PHaWCyc49SEGSLn90iT+ZFQ==': {
-      id: 'AVn5tu8PHaWCyc49SEGSLn90iT+ZFQ==',
-      tasks: ['AVn5tu8eSu3b1GmqQvG+5cDaoORotQ==']
-    }
-  }), {
-    type: REMOVE_TASK,
-    serverId: 'AVn5tu8PHaWCyc49SEGSLn90iT+ZFQ==',
-    taskId: 'AVn5tu8eSu3b1GmqQvG+5cDaoORotQ=='
+    expect(state.toJS()).toEqual({
+      'AVn5d/fFT13LWVVYQzKEVy1VkdW4vQ==': {
+        tasks: ['AVn5d/fWdqw6Cki7TYu1mFutjHiDEQ==']
+      }
+    });
   });
 
-  expect(state.toJS()).toEqual({
-    'AVn5tu8PHaWCyc49SEGSLn90iT+ZFQ==': {
-      id: 'AVn5tu8PHaWCyc49SEGSLn90iT+ZFQ==',
-      tasks: []
-    }
+  test(`${REMOVE_TASK} action`, () => {
+    const state = reducer(fromJS({
+      'AVn5tu8PHaWCyc49SEGSLn90iT+ZFQ==': {
+        tasks: ['AVn5tu8eSu3b1GmqQvG+5cDaoORotQ==']
+      }
+    }), {
+      type: REMOVE_TASK,
+      serverId: 'AVn5tu8PHaWCyc49SEGSLn90iT+ZFQ==',
+      taskId: 'AVn5tu8eSu3b1GmqQvG+5cDaoORotQ=='
+    });
+
+    expect(state.toJS()).toEqual({
+      'AVn5tu8PHaWCyc49SEGSLn90iT+ZFQ==': {
+        tasks: []
+      }
+    });
   });
 });
