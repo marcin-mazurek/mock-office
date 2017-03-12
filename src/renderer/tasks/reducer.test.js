8ebd587abec9e6878c2f931f3ee9754e2a9a56cd
Bartosz Adamczyk <born2perform1@gmail.com>
2017-03-12 23:02:02 +0100

    test use describe to structure tasks reducer tests

--- src/renderer/tasks/reducer.test.js
+++ src/renderer/tasks/reducer.test.js
@@ -3,30 +3,37 @@ import reducer from './reducer';
 import { REMOVE } from './removeTask/actions';
 import { ADD } from './addTask/actions';
 
-test(`${ADD} reducer`, () => {
-  const state = reducer(fromJS({}), {
-    type: ADD,
-    taskPayload: { body: 'task for some-url-3' },
-    taskId: 'AVn5d/fWdqw6Cki7TYu1mFutjHiDEQ=='
+describe('tasks reducer', () => {
+  test(`${ADD} action`, () => {
+    const state = reducer(fromJS({}), {
+      type: ADD,
+      taskPayload: { body: 'task for some-url-3' },
+      taskId: 'AVn5d/fWdqw6Cki7TYu1mFutjHiDEQ=='
+    });
+
+    expect(state.toJS()).toEqual({
+      'AVn5d/fWdqw6Cki7TYu1mFutjHiDEQ==': {
+        taskPayload: {
+          body: 'task for some-url-3',
+        },
+        id: 'AVn5d/fWdqw6Cki7TYu1mFutjHiDEQ=='
+      }
+    });
   });
 
-  expect(state.toJS()).toEqual({
-    'AVn5d/fWdqw6Cki7TYu1mFutjHiDEQ==': {
-      taskPayload: {
-        body: 'task for some-url-3',
-      },
-      id: 'AVn5d/fWdqw6Cki7TYu1mFutjHiDEQ=='
-    }
+  test(`${REMOVE} action`, () => {
+    const state = reducer(fromJS({
+      'AVn5tu8eSu3b1GmqQvG+5cDaoORotQ==': {
+        taskPayload: { data: 'task for some-url-3' },
+        id: 'AVn5tu8eSu3b1GmqQvG+5cDaoORotQ=='
+      }
+    }), { type: REMOVE, taskId: 'AVn5tu8eSu3b1GmqQvG+5cDaoORotQ==' });
+
+    expect(state.toJS()).toEqual({});
   });
-});
 
-test(`${REMOVE} reducer`, () => {
-  const state = reducer(fromJS({
-    'AVn5tu8eSu3b1GmqQvG+5cDaoORotQ==': {
-      taskPayload: { data: 'task for some-url-3' },
-      id: 'AVn5tu8eSu3b1GmqQvG+5cDaoORotQ=='
-    }
-  }), { type: REMOVE, taskId: 'AVn5tu8eSu3b1GmqQvG+5cDaoORotQ==' });
-
-  expect(state.toJS()).toEqual({});
+  test('unknown action', () => {
+    const state = {};
+    expect(reducer(state, { type: 'unknown action type' })).toBe(state);
+  });
 });
