8d2478f25abdb94e3b6c65705c48e4d0ee2fea07
clix <born2perform1@gmail.com>
2017-03-30 16:06:45 +0200

    test cover ScenePart

---
+++ src/main/scenario/ScenePart.test.js
@@ -0,0 +1,92 @@
+import { Subscription } from 'rxjs';
+import ScenePart from './ScenePart';
+import { ServerEventsEmitter } from '../globalEvents';
+
+describe('ScenePart', () => {
+  it('should setup emitter', () => {
+    const scenePart = new ScenePart({ emitter: new ServerEventsEmitter() });
+    expect(scenePart.emitter).toBeInstanceOf(ServerEventsEmitter);
+  });
+
+  describe('play', () => {
+    it('should change pending state if it is not pending yet', () => {
+      const scenePart = new ScenePart({
+        emitter: new ServerEventsEmitter(),
+        scheduleDetails: {
+          type: 'immediate',
+          payload: { key: 'value' }
+        }
+      });
+
+      scenePart.play(() => {
+      }).then(() => {
+        expect(scenePart.pending).toBeTruthy();
+      });
+    });
+
+    it('should setup functions for canceling if it is not pending yet', () => {
+      const scenePart = new ScenePart({
+        emitter: new ServerEventsEmitter(),
+        scheduleDetails: {
+          type: 'immediate',
+          payload: { key: 'value' }
+        }
+      });
+
+      scenePart.play(() => {
+      }).then(() => {
+        expect(scenePart.subscription).toBeInstanceOf(Subscription);
+        expect(scenePart.stop).toBeInstanceOf(Function);
+      });
+    });
+
+    it('should return rejected promise if is pending', (done) => {
+      const scenePart = new ScenePart({
+        emitter: new ServerEventsEmitter(),
+        scheduleDetails: {
+          type: 'immediate',
+          payload: { key: 'value' }
+        }
+      });
+
+      scenePart.pending = true;
+
+      scenePart.play(() => {})
+        .catch((reason) => {
+          expect(reason).toEqual('Pending ScenePart cant be played.');
+        })
+        .then(() => {
+          done();
+        });
+    });
+  });
+  describe('cancel', () => {
+    it('should call stop only if scene part is pending', () => {
+      const scenePart = new ScenePart({
+        emitter: new ServerEventsEmitter(),
+        scheduleDetails: {
+          type: 'immediate',
+          payload: { key: 'value' }
+        }
+      });
+      scenePart.play = function play() {
+        this.pending = true;
+        this.stop = jest.fn();
+      };
+      scenePart.play(() => {});
+      scenePart.cancel();
+      expect(scenePart.stop).toHaveBeenCalled();
+
+      const scenePartNotPending = new ScenePart({
+        emitter: new ServerEventsEmitter(),
+        scheduleDetails: {
+          type: 'immediate',
+          payload: { key: 'value' }
+        }
+      });
+      scenePartNotPending.stop = jest.fn();
+      scenePartNotPending.cancel();
+      expect(scenePartNotPending.stop).not.toHaveBeenCalled();
+    });
+  });
+});
