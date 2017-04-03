77a169a4746dbdee7980ea3bf9554cfc310574f5
Bartosz Adamczyk <born2perform1@gmail.com>
2017-04-03 15:02:38 +0200

    test update routes tests

--- src/renderer/routes/Main.test.js
+++ src/renderer/routes/Main.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 import Main from './Main';
 
-test('Main should render', () => {
-  const wrapper = shallow(<Main />);
-  expect(toJson(wrapper)).toMatchSnapshot();
+describe('Main', () => {
+  test('should render children', () => {
+    const Component = () => <div />;
+    const wrapper = shallow(<Main><Component /></Main>);
+    expect(wrapper.find(Component)).toHaveLength(1);
+  });
 });
