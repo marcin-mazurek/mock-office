88058c09cd6eb0e36dc14f095fe01302c1667839
Bartosz Adamczyk <born2perform1@gmail.com>
2017-04-04 14:54:59 +0200

    test cover ErrorList component with tests

---
+++ src/renderer/errors/ErrorList.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { List } from 'immutable';
+import toJson from 'enzyme-to-json';
+import { Provider } from 'react-redux';
+import configureStore from '../store';
+import ErrorListConnect, { ErrorList } from './ErrorList';
+import AppError from '../entities/errors/AppError';
+import { add } from '../entities/errors/actions';
+
+describe('ErrorList', () => {
+  it('should display reason', () => {
+    const errors = List.of(new AppError('reason 1'));
+    const wrapper = shallow(
+      <ErrorList errors={errors} remove={() => {}} />
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should display remove button', () => {
+    const err = new AppError('reason 1');
+    const errors = List.of(err);
+    const wrapper = shallow(
+      <ErrorList errors={errors} remove={() => {}} />
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
+
+describe('ErrorListConnect', () => {
+  it('should display current errors from redux store', () => {
+    const store = configureStore();
+    const wrapper = mount(
+      <Provider store={store}>
+        <ErrorListConnect />
+      </Provider>
+    );
+    const errorList = wrapper.find(ErrorList);
+    expect(toJson(errorList)).toMatchSnapshot();
+    store.dispatch(add('reason 1'));
+    expect(toJson(errorList)).toMatchSnapshot();
+    errorList.find('li').first().find('button').simulate('click');
+    expect(toJson(errorList)).toMatchSnapshot();
+  });
+});
