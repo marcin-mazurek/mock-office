9cc74527f2b4eaa6d779f1d68fc8fbc515df0a76
Bartosz Patkaniowski <bartosz.patkaniowski@grandparade.co.uk>
2017-04-06 17:03:02 +0200

    feat Add EditableField component

--- stories/index.js
+++ stories/index.js
@@ -1,3 +1,4 @@
 import '../src/renderer/styles/main.scss';
+import '../src/renderer/editableField/EditableField.story';
 import '../src/renderer/sidebar/SideBarServers.story';
 import '../src/renderer/servers/inspectServer/Server.story';
