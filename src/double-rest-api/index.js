9ef807a62af7b60cd055c01e5c30a5d579082af4
Bartosz Adamczyk <born2perform1@gmail.com>
2017-04-08 23:18:10 +0200

    feat declare paths

---
+++ src/double-rest-api/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const app = express();
+
+app.post('/add-server', (req, res) => {
+  res.end();
+});
+app.post('/remove-server', (req, res) => {
+  res.end();
+});
+app.post('/add-scene', (req, res) => {
+  res.end();
+});
+app.post('/remove-scene', (req, res) => {
+  res.end();
+});
+app.post('/start-server', (req, res) => {
+  res.end();
+});
+app.post('/stop-server', (req, res) => {
+  res.end();
+});
