bde19d86becc7dccd66e417ea5f8f75cd6688fc6
Bartosz Adamczyk <born2perform1@gmail.com>
2017-04-08 23:25:29 +0200

    feat implement handlers

--- src/double-rest-api/index.js
+++ src/double-rest-api/index.js
@@ -1,22 +1,38 @@
 import express from 'express';
+import { Double } from '../double';
 
 const app = express();
+const double = new Double();
 
 app.post('/add-server', (req, res) => {
-  res.end();
+  const id = double.add(req.body);
+  res.send(id);
 });
+
 app.post('/remove-server', (req, res) => {
+  double.remove(req.body.id);
   res.end();
 });
+
 app.post('/add-scene', (req, res) => {
-  res.end();
+  const server = double.find(req.body.serverId);
+  const sceneId = server.getScenario().addScene(req.body.scene);
+  res.end(sceneId);
 });
+
 app.post('/remove-scene', (req, res) => {
+  double.find(req.body.serverId).getScenario().removeScene(req.body.sceneId);
   res.end();
 });
+
 app.post('/start-server', (req, res) => {
-  res.end();
+  double.find(req.body.id).start(() => {
+    res.end();
+  });
 });
+
 app.post('/stop-server', (req, res) => {
-  res.end();
+  double.find(req.body.id).stop(() => {
+    res.end();
+  });
 });
