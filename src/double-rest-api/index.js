5247880ad989f2e0cd4ea9e70fa1f45c1acbef80
Bartosz Adamczyk <born2perform1@gmail.com>
2017-04-10 11:10:42 +0200

    feat implement validation for /add-server

--- src/double-rest-api/index.js
+++ src/double-rest-api/index.js
@@ -1,12 +1,59 @@
+import Ajv from 'ajv';
 import express from 'express';
-import { Double } from '../double';
+import bodyParser from 'body-parser';
+import Double from '../double';
 
+const ajv = new Ajv();
 const app = express();
 const double = new Double();
 
-app.post('/add-server', (req, res) => {
-  const id = double.add(req.body);
-  res.send(id);
+app.post('/add-server', bodyParser.json(), (req, res) => {
+  const schema = {
+    properties: {
+      name: {
+        type: 'string'
+      },
+      port: {
+        type: 'number'
+      },
+      type: {
+        type: 'string',
+        enum: ['http', 'ws']
+      },
+      isSecure: {
+        type: 'boolean'
+      },
+      keyPath: {
+        type: 'string'
+      },
+      certPath: {
+        type: 'string'
+      }
+    },
+    required: [
+      'name',
+      'type'
+    ]
+  };
+
+  if (ajv.validate(schema, req.body)) {
+    const id = double.add(
+      req.body.name,
+      req.body.port,
+      req.body.type,
+      req.body.isSecure,
+      req.body.keyPath,
+      req.body.certPath
+    );
+
+    res.json({
+      id
+    });
+
+    return;
+  }
+
+  res.json(ajv.errors);
 });
 
 app.post('/remove-server', (req, res) => {
