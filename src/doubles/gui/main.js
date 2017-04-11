b21a0a584744b19bd18ca8c649f0c971bf1022dc
Bartosz Adamczyk <born2perform1@gmail.com>
2017-04-11 22:35:02 +0200

    fix dont sync twice state

--- src/doubles/gui/main.js
+++ src/doubles/gui/main.js
@@ -1,6 +1,5 @@
 import { render } from 'react-dom';
 import syncScenes from './app/syncScenes';
-import loadState from './app/loadState';
 import configureStore from './store';
 import routes from './routes';
 import configureApp from './app';
@@ -9,6 +8,5 @@ const store = configureStore();
 const App = configureApp(store, routes);
 
 syncScenes(store);
-loadState(store);
 
 render(App, document.querySelector('.js-main'));
