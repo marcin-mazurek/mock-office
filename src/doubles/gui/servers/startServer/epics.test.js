d870396e2c7a882854599feebedccc469dc3844d
Bartosz Adamczyk <born2perform1@gmail.com>
2017-04-12 12:16:24 +0200

    test write unit tests for startServerEpic

--- src/doubles/gui/servers/startServer/epics.test.js
+++ src/doubles/gui/servers/startServer/epics.test.js
@@ -1,14 +1,40 @@
-import configureMockStore from 'redux-mock-store';
-import { createEpicMiddleware } from 'redux-observable';
-import { START } from '../../entities/servers/actions';
-import initStart from './actions';
-
+/* eslint-disable global-require */
 describe('startServerEpic', () => {
-  it(`should dispatch ${START}`, () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.resetModules();
+  });
+
+  test('actions snapshot when response object hasn\'t got errors', () => {
+    const configureMockStore = require('redux-mock-store').default;
+    const createEpicMiddleware = require('redux-observable').createEpicMiddleware;
+    const initStart = require('./actions').default;
     jest.mock('../../api/api', () => ({
       requestStartServer: () => ([{ id: 'some id' }])
     }));
     // eslint-disable-next-line global-require
+    const startServerEpic = require('./epics').default;
+
+    const epicMiddleware = createEpicMiddleware(startServerEpic);
+    const mockStore = configureMockStore([epicMiddleware]);
+    const store = mockStore();
+    store.dispatch(initStart('some id'));
+    expect(store.getActions()).toMatchSnapshot();
+  });
+
+  it('actions snapshot when response object has got errors', () => {
+    const configureMockStore = require('redux-mock-store').default;
+    const createEpicMiddleware = require('redux-observable').createEpicMiddleware;
+    const initStart = require('./actions').default;
+    jest.mock('../../api/api', () => ({
+      requestStartServer: () => [{
+        errors: [
+          {
+            message: 'error message'
+          }
+        ]
+      }]
+    }));
     const startServerEpic = require('./epics').default;
 
     const epicMiddleware = createEpicMiddleware(startServerEpic);
