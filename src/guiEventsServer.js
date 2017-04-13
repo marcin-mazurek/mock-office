a45af08270e6032444fca54df3ed2682e58d11df
Bartosz Adamczyk <born2perform1@gmail.com>
2017-04-13 10:42:21 +0200

    refactor make guiEventsServer module consistent with other servers

---
+++ src/guiEventsServer.js
@@ -0,0 +1,79 @@
+import http from 'http';
+import { Server as WebSocketServer } from 'ws';
+import { addListener } from './servers-manager/emitter';
+
+export function configureGuiEventsServer(serversManager, persistentState) {
+  const httpServer = http.createServer();
+  const server = new WebSocketServer({ server: httpServer });
+  const sockets = [];
+
+  function broadcast(event, args) {
+    sockets.forEach(
+      socket => socket.send(JSON.stringify(Object.assign({ event }, args)))
+    );
+  }
+
+  function autoHydrateClientState(ws) {
+    ws.send(
+      JSON.stringify(
+        Object.assign(
+          { event: 'RESTORE_STATE' },
+          { state: serversManager.getState() }
+        )
+      )
+    );
+  }
+
+  function watchSaveState(ws) {
+    ws.on('message', (message) => {
+      if (message === 'SAVE_STATE') {
+        persistentState.save();
+      }
+    });
+  }
+
+  server.on('connection', autoHydrateClientState);
+  server.on('connection', watchSaveState);
+
+  server.on('connection', (ws) => {
+    sockets.push(ws);
+  });
+
+  addListener('SCENE_REMOVED',
+    args => broadcast('SCENE_REMOVED', args)
+  );
+
+  addListener('SCENE_START',
+    args => broadcast('SCENE_START', args)
+  );
+
+  addListener('SCENE_END',
+    args => broadcast('SCENE_END', args)
+  );
+
+  addListener('SCENE_CANCEL',
+    args => broadcast('SCENE_CANCEL', args)
+  );
+
+  addListener('SCENE_REMOVED_AFTER_USE',
+    args => broadcast('SCENE_REMOVED_AFTER_USE', args)
+  );
+
+  addListener('SCENE_PART_START',
+    args => broadcast('SCENE_PART_START', args)
+  );
+
+  addListener('SCENE_PART_END',
+    args => broadcast('SCENE_PART_END', args)
+  );
+
+  addListener('SCENE_PART_CANCEL',
+    args => broadcast('SCENE_PART_CANCEL', args)
+  );
+
+  return httpServer;
+}
+
+export function serveGuiEventsServer(server, port) {
+  server.listen(port);
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import commandLineArgs from 'command-line-args';
 import { serveAppServer, createAppServer } from './appServer';
 import createPersistentState from './createPersistentState';
-import createGuiEventsServer from './createGuiEventsServer';
+import { configureGuiEventsServer, serveGuiEventsServer } from './guiEventsServer';
 import ServersManager from './servers-manager';
 import { createGuiServer, serveGuiServer } from './guiServer';
 
 const APP_SERVER_PORT = 3060;
 const GUI_SERVER_PORT = 3070;
+const GUI_EVENTS_SERVER_PORT = 3061;
 const serversManager = new ServersManager();
 const persistentState = createPersistentState(serversManager);
 persistentState.restore();
@@ -23,7 +24,8 @@ const options = commandLineArgs([
 // Spawning GUI server
 if (options.gui) {
   // eslint-disable-next-line global-require
-  const guiEventsServer = createGuiEventsServer(serversManager, persistentState);
-  guiEventsServer.start(3061);
+  serveGuiEventsServer(
+    configureGuiEventsServer(serversManager, persistentState), GUI_EVENTS_SERVER_PORT
+  );
   serveGuiServer(createGuiServer(), GUI_SERVER_PORT);
 }
