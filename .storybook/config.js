490e815118376275bfee041831866dda8c4fcf84
Bartosz Adamczyk <born2perform1@gmail.com>
2017-04-13 18:19:45 +0200

    fix update path to stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,8 @@
 import { configure } from '@kadira/storybook';
-import '../src/doubles/gui/styles/main.scss';
+import '../src/gui/styles/main.scss';
 
 function loadStories() {
-  require('../src/doubles/gui/.storybook/stories');
+  require('../src/gui/.storybook/stories');
 }
 
 configure(loadStories, module);
