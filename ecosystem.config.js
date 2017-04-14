0f544cb1a4a6b97eac7f608f5ed29e9624c6e50e
Bartosz Adamczyk <born2perform1@gmail.com>
2017-04-14 16:46:47 +0200

    chore improve development process
    - add pm2 ecosystem config: 2 apps, one with gui

---
+++ ecosystem.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+
+    // App server only
+    {
+      name: 'app-server',
+      script: './dist/index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      watch: [
+        './dist'
+      ],
+      out_file: __dirname + '/pm2.log',
+      error_file: __dirname + '/pm2-errors.log',
+      combine_logs: true
+    },
+    // with GUI
+    {
+      name: 'app-server-with-gui',
+      script: './dist/index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      watch: [
+        './dist'
+      ],
+      out_file: __dirname + '/pm2.log',
+      error_file: __dirname + '/pm2-errors.log',
+      args: "--gui",
+      combine_logs: true
+    }
+  ]
+};
