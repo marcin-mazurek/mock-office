e7abb39a669c54853de9d7748d5459d3dc238942
Bartosz Adamczyk born2perform1@gmail.com <born2perform1@gmail.com>
2017-05-10 12:39:18 +0200

    feat(add server modal): implement react component

---
+++ src/gui/servers/addServer/modal/AddServerModal.story.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { storiesOf } from '@kadira/storybook';
+import AddServerModal from './AddServerModal';
+import configureStore from '../../../store';
+
+storiesOf('AddServerModal', module)
+  .add('default', () => {
+    const store = configureStore();
+
+    return (
+      <Provider store={store}>
+        <AddServerModal />
+      </Provider>
+    );
+  });
