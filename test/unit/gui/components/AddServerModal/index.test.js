0649999facc3a2dbba07eed14ec6de9a1f7951a5
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2017-09-05 15:50:22 +0200

    test(add server): test AddServerModal

---
+++ test/unit/gui/components/AddServerModal/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import AddServerModal from '../../../../../src/gui/components/AddServerModal';
+
+describe('AddServerModal', () => {
+  describe('snapshots', () => {
+    test('default', () => {
+      const wrapper = shallow(
+        <AddServerModal />
+      );
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
+  });
+});
