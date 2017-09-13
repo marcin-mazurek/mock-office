c1087a56582c0b5378821dc7aa6eda94948465ea
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2017-09-13 17:47:36 +0200

    chore: linting, transpiling setup improvements

--- src/cli/chan.js
+++ src/cli/chan.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 
-/* eslint no-console:off */
-"use strict";
-console.log(process.argv);
-const guiEnabled = (process.argv.indexOf("--gui") > -1);
+const guiEnabled = (process.argv.indexOf('--gui') > -1);
 const chan = require('../index');
 
 chan.startApp();
