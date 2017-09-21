3a918d1784f624c791319044c518bff493eadbfe
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2017-09-21 14:13:31 +0200

    feat(modal): add animation to modal

--- src/lib/gui/components/Modal/Modal.js
+++ src/lib/gui/components/Modal/Modal.js
@@ -1,24 +1,77 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TransitionMotion, spring } from 'react-motion';
 
 const createModal = (types) => {
-  const Modal = ({ component, onOverlayClick }) => {
-    const ModalComponent = types[component];
-    const isOpen = !!ModalComponent;
+  class Modal extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        items: []
+      };
+    }
 
-    return isOpen
-      ? (
-        <div
-          className="modal"
+    componentWillReceiveProps(props) {
+      const { component } = props;
+      const ModalComponent = types[component];
+      const isOpen = !!ModalComponent;
+
+      if (isOpen) {
+        this.setState({
+          items: this.state.items.concat([{ key: 'modal', style: { opacity: spring(1) } }])
+        });
+      } else {
+        this.setState({
+          items: []
+        });
+      }
+    }
+
+    willEnter() {
+      return { opacity: 0 };
+    }
+
+    willLeave() {
+      return { opacity: spring(0) };
+    }
+
+    render() {
+      const { component, onOverlayClick } = this.props;
+      const ModalComponent = types[component];
+      const isOpen = !!ModalComponent;
+
+      return (
+        <TransitionMotion
+          willEnter={this.willEnter}
+          willLeave={this.willLeave}
+          styles={this.state.items}
         >
-          <button className="modal__overlay" onClick={onOverlayClick} />
-          <div className="modal__content">
-            <ModalComponent />
-          </div>
-        </div>
-      )
-      : null;
-  };
+          {
+            interpolatingStyles =>
+              <div>
+                {
+                  interpolatingStyles.map(config =>
+                    <div
+                      key={config.key}
+                      className="modal"
+                      style={config.style}
+                    >
+                      <button className="modal__overlay" onClick={onOverlayClick} />
+                      <div className="modal__content">
+                        {
+                          isOpen ? <ModalComponent /> : null
+                        }
+                      </div>
+                    </div>
+                  )
+                }
+              </div>
+          }
+        </TransitionMotion>
+      );
+    }
+  }
+
   Modal.propTypes = {
     component: PropTypes.string,
     onOverlayClick: PropTypes.func.isRequired
