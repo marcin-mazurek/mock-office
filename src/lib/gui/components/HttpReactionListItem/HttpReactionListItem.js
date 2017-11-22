474fc8683b5afabfae3b1c77ac16112f6ee5f381
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2017-11-22 21:42:15 +0100

    feat: improve look of the http reaction list item

--- src/lib/gui/components/HttpReactionListItem/HttpReactionListItem.js
+++ src/lib/gui/components/HttpReactionListItem/HttpReactionListItem.js
@@ -3,31 +3,20 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 
 const HttpReactionListItem = ({ reaction }) => {
   const delay = reaction.getIn(['schedule', 'delay']);
-  const interval = reaction.getIn(['schedule', 'interval']);
+
   return (
     <div className="reaction-list-item">
-      <div className="reaction-list-item__params">
-        <div className="reaction-list-item__tag">
-          {reaction.getIn(['params', 'status'])}
+      <div className="reaction-list-item__event-property">
+        <div className="reaction-list-item__event-property-label">Status</div>
+        <div className="reaction-list-item__event-property-value">
+          { reaction.getIn(['params', 'status']) || 'Any' }
         </div>
-        {
-          delay
-            ? (
-              <div className="reaction-list-item__tag">
-                <i className="fa fa-hourglass-o" />{' '}{delay / 1000}{'s'}
-              </div>
-            )
-            : null
-        }
-        {
-          interval
-            ? (
-              <div className="reaction-list-item__tag">
-                <i className="fa fa-history" />{' '}{interval / 1000}{'s'}
-              </div>
-            )
-            : null
-        }
+      </div>
+      <div className="reaction-list-item__event-property">
+        <div className="reaction-list-item__event-property-label">Delay</div>
+        <div className="reaction-list-item__event-property-value">
+          { delay || 'No' }
+        </div>
       </div>
     </div>
   );
